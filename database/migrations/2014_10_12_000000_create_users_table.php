f51551f0701c509463d3d22127a28795a2d6e078
kianhvatianan <kiannarahvatiannan@gmail.com>
2025-01-12 15:58:50 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 return new class extends Migration {
     public function up(): void
     {
@@ -15,6 +18,16 @@ return new class extends Migration {
             $table->enum('role', ['admin', 'member'])->default('member');
             $table->timestamps();
         });
+
+         // Insert admin data after creating the table
+         DB::table('users')->insert([
+            'name' => 'Yang Kuasa',
+            'email' => 'yangmahakuasa@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     public function down(): void
